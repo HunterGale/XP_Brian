dcbdfcff0d1977b0a0f9ddb2522f6209bc95cd22
Hunter Gale <hunter137012@gmail.com>
2023-04-14 14:25:00 -0400

    Adding secret tests

--- XtremePTest/test.cpp
+++ XtremePTest/test.cpp
@@ -1,6 +1,37 @@
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include <gtest/gtest.h>
+#include <string>
+#include "secret_cipher.h" // include the header file with the function declaration
+
+TEST(SecretCipherTest, Test1) {
+	std::string input = "HELLO WORLD";
+	std::string expected_output = "HOLELWRDLO";
+	int key = 3;
+	std::string actual_output = secret_cipher(input, key);
+	EXPECT_EQ(expected_output, actual_output);
+}
+
+TEST(SecretCipherTest, Test2) {
+	std::string input = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+	std::string expected_output = "TBJRDHKRXUETYOECOOMVHZGQIWFPOEAUNSL";
+	int key = 5;
+	std::string actual_output = secret_cipher(input, key);
+	EXPECT_EQ(expected_output, actual_output);
+}
+
+TEST(SecretCipherTest, Test3) {
+	std::string input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	std::string expected_output = "ACEGIKMOQSUWYBDFHJLNPRTVXZ";
+	int key = 2;
+	std::string actual_output = secret_cipher(input, key);
+	EXPECT_EQ(expected_output, actual_output);
+}
+
+TEST(SecretCipherTest, CaseTest) {
+	std::string input = "hello world";
+	std::string expected_output = "HOLELWRDLO";
+	int key = 3;
+	std::string actual_output = secret_cipher(input, key);
+	EXPECT_EQ(expected_output, actual_output);
 }
